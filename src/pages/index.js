4f250c2490d977e70d4c044ea09f38333d2602c0
Georgina Howe <georginahowe15@gmail.com>
2023-10-19 17:14:16 +0100

    Fix import typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Layout from "../components/Layout/layout";
+import Layout from "../components/Layout/Layout";
 import { graphql, useStaticQuery } from "gatsby";
 //import RatingBox from "../components/RatingBox/RatingBox";
 import RatingDropdown from '../components/RatingDropdown/RatingDropdown';
